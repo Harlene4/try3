fd5de4759a6db2c4f20fb82cd8330491bf0f04c3
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 18:31:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head> <TITLE> MAJOR PT </TITLE>
+</head>
+<framset cols= "50%, 50%>
+<frameset src= "">
+<frameset src= "https://homew3b.herokuapp.com/">
+<frameset rows= "50%,  50%">
+<frame src= "https://pandemictoday.herokuapp.com/">
+<frame src= "https://mathtomorrow.herokuapp.com/">
+</frameset>
+</html>
