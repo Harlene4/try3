ab2027235cb5b6792166c3d9ac99a5ee9514d870
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 18:36:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <head> <TITLE> MAJOR PT </TITLE>
 </head>
 <framset cols= "50%, 50%>
-<frameset src= "">
-<frameset src= "https://homew3b.herokuapp.com/">
+<frame src= "">
+<frame src= "https://homew3b.herokuapp.com/">
 <frameset rows= "50%,  50%">
 <frame src= "https://pandemictoday.herokuapp.com/">
 <frame src= "https://mathtomorrow.herokuapp.com/">
